71cbc2080da00081b49dc7934a1ac483100d9444
piekvossen <piekvossen@gmail.com>
2021-11-11 08:49:53 +0100

    brain api notebook

--- src/chatbots/intentions/talk.py
+++ src/chatbots/intentions/talk.py
@@ -17,40 +17,71 @@ if src_path not in sys.path:
 import chatbots.util.capsule_util as c_util
 
 
-def process_text_and_think(scenario: Scenario,
-                           place_id: str,
-                           location: str,
-                           textSignal: TextSignal,
-                           human_id: str,
-                           my_brain: LongTermMemory,
-                           replier: LenkaReplier):
-    chat = Chat(human_id)
-    chat.add_utterance([UtteranceHypothesis(c_util.seq_to_text(textSignal.seq), 1.0)])
-    chat.last_utterance.analyze()
-    # No triple was extracted, so we missed three items (s, p, o)
 
-    if chat.last_utterance.triple is None:
-        reply = "Any gossip?" + '\n'
-    else:
-        # A triple was extracted so we compare it elementwise
-        capsule = c_util.scenario_utterance_and_triple_to_capsule(scenario,
-                                                                  place_id,
-                                                                  location,
-                                                                  textSignal,
-                                                                  human_id,
-                                                                  chat.last_utterance.type,
-                                                                  chat.last_utterance.perspective,
-                                                                  chat.last_utterance.triple)
+# basic function that creates a capsule from a triple and stores it on the brain
+# @parameters triple as a json string and the initilised brain as LongTermMemory
+# Posting triples triggers thoughts that are converted to json and returned
+# ADDITIONAL PARAMETERS  my_brain.update(capsule, reason_types=True, create_label=False)
+# reason_types=True --> it will find subjects and objects in the Semantic web and add the type information
+# create_label=True --> it will automatically create a rdfs:label property from the subject label
 
-        response = my_brain.update(capsule, reason_types=True, create_label=False)
-        response_json = brain_response_to_json(response)
+def post_a_triple(triple, my_brain: LongTermMemory):
+    response_json = None
+    capsule = c_util.triple_to_capsule(triple, UtteranceType.STATEMENT)
+    response = my_brain.update(capsule)
+    response_json = brain_response_to_json(response)
+    return capsule, response_json
 
-        if replier:
-            reply = replier.reply_to_statement(response_json, proactive=True, persist=False)
-        else:
-            reply = "Any gossip?" + '\n'
+def post_a_triple_label_and_type(triple, my_brain: LongTermMemory):
+    response_json = None
+    capsule = c_util.triple_to_capsule(triple, UtteranceType.STATEMENT)
+    response = my_brain.update(capsule, reason_types=True, create_label=True)
+    response_json = brain_response_to_json(response)
+    return capsule, response_json
+
+def post_a_triple_and_verbalise_throughts(triple, replier: LenkaReplier, my_brain: LongTermMemory):
+    reply = None
+    capsule = c_util.triple_to_capsule(triple, UtteranceType.STATEMENT)
+    print(capsule)
+    response = my_brain.update(capsule) # ADDITIONAL PARAMTERS reason_types=True, create_label=False
+    response_json = brain_response_to_json(response)
+    reply = replier.reply_to_statement(response_json) # ADDITIONAL PARAMETERS proactive=True, persist=True
+    
+    if reply is None:
+        reply = choice(ELOQUENCE)
 
     return reply
+
+
+
+def post_a_query(triple,my_brain: LongTermMemory):
+    response_json = None
+
+    capsule = c_util.triple_to_capsule(triple, UtteranceType.QUESTION)
+    print(capsule)
+    response = my_brain.query_brain(capsule)
+    response_json = brain_response_to_json(response)
+
+    return response_json
+
+
+def post_a_query_and_verbalise_answer(triple, replier: LenkaReplier, my_brain: LongTermMemory):
+    reply = None
+
+    capsule = c_util.triple_to_capsule(triple, UtteranceType.QUESTION)
+    print(capsule)
+    response = my_brain.query_brain(capsule)
+    response_json = brain_response_to_json(response)
+    reply = replier.reply_to_question(response_json)
+
+    if reply is None:
+        reply = choice(ELOQUENCE)
+
+    return reply
+
+
+
+######## Utility function to integrate calls to the brain with ESMISSOR scenarios
 
 
 def process_text_and_reply(scenario: Scenario,
@@ -86,7 +117,7 @@ def process_text_and_reply(scenario: Scenario,
             reply = replier.reply_to_question(response_json)
 
         if chat.last_utterance.type == UtteranceType.STATEMENT:
-            response = my_brain.update(capsule, reason_types=True, create_label=False)
+            response = my_brain.update(capsule, reason_types=True, create_label=True)
             response_json = brain_response_to_json(response)
             reply = replier.reply_to_statement(response_json, proactive=True, persist=True)
 
@@ -96,41 +127,39 @@ def process_text_and_reply(scenario: Scenario,
     return reply
 
 
-def answer_a_query(triple, replier: LenkaReplier, my_brain: LongTermMemory):
-    reply = None
 
-    capsule = c_util.triple_to_capsule(triple, UtteranceType.QUESTION)
-    print(capsule)
-    response = my_brain.query_brain(capsule)
-    response_json = brain_response_to_json(response)
-    reply = replier.reply_to_question(response_json)
+def process_text_and_think(scenario: Scenario,
+                           place_id: str,
+                           location: str,
+                           textSignal: TextSignal,
+                           human_id: str,
+                           my_brain: LongTermMemory,
+                           replier: LenkaReplier):
+    chat = Chat(human_id)
+    chat.add_utterance([UtteranceHypothesis(c_util.seq_to_text(textSignal.seq), 1.0)])
+    chat.last_utterance.analyze()
+    # No triple was extracted, so we missed three items (s, p, o)
 
-    if reply is None:
-        reply = choice(ELOQUENCE)
+    if chat.last_utterance.triple is None:
+        reply = "Any gossip?" + '\n'
+    else:
+        # A triple was extracted so we compare it elementwise
+        capsule = c_util.scenario_utterance_and_triple_to_capsule(scenario,
+                                                                  place_id,
+                                                                  location,
+                                                                  textSignal,
+                                                                  human_id,
+                                                                  chat.last_utterance.type,
+                                                                  chat.last_utterance.perspective,
+                                                                  chat.last_utterance.triple)
 
-    return reply
+        response = my_brain.update(capsule, reason_types=True, create_label=False)
+        response_json = brain_response_to_json(response)
 
-# basic function that creates a capsule from a triple and stores it on the brain
-# @parameters triple as a json string and the initilised brain as LongTermMemory
-# Posting triples triggers thoughts that are converted to json and returned
-def post_a_triple_and_get_thoughts(triple, my_brain: LongTermMemory):
-    response_json = None
-    capsule = c_util.triple_to_capsule(triple, UtteranceType.STATEMENT)
-    response = my_brain.update(capsule)
-    response_json = brain_response_to_json(response)
-    return capsule, response_json
-
-# ADDITIONAL PARAMETERS  my_brain.update(capsule, reason_types=True, create_label=False)
-
-def post_a_triple_and_verbalise_throughts(triple, replier: LenkaReplier, my_brain: LongTermMemory):
-    reply = None
-    capsule = c_util.triple_to_capsule(triple, UtteranceType.STATEMENT)
-    print(capsule)
-    response = my_brain.update(capsule) # ADDITIONAL PARAMTERS reason_types=True, create_label=False
-    response_json = brain_response_to_json(response)
-    reply = replier.reply_to_statement(response_json) # ADDITIONAL PARAMETERS proactive=True, persist=True
-    
-    if reply is None:
-        reply = choice(ELOQUENCE)
+        if replier:
+            reply = replier.reply_to_statement(response_json, proactive=True, persist=False)
+        else:
+            reply = "Any gossip?" + '\n'
 
     return reply
+
