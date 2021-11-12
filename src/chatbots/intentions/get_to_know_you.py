a5a64df4c51f5f2d8f3de395cd5310bc5a36a9d2
Taewoon Kim <tae898@gmail.com>
2021-11-12 22:54:04 +0100

    Fix stuff

--- src/chatbots/intentions/get_to_know_you.py
+++ src/chatbots/intentions/get_to_know_you.py
@@ -4,7 +4,14 @@ from cltl.triple_extraction.api import Chat, UtteranceHypothesis
 from emissor.persistence import ScenarioStorage
 from emissor.representation.annotation import AnnotationType, Token, NER
 from emissor.representation.container import Index
-from emissor.representation.scenario import Modality, ImageSignal, TextSignal, Mention, Annotation, Scenario
+from emissor.representation.scenario import (
+    Modality,
+    ImageSignal,
+    TextSignal,
+    Mention,
+    Annotation,
+    Scenario,
+)
 from cltl.brain.long_term_memory import LongTermMemory
 from cltl.combot.backend.api.discrete import UtteranceType
 import chatbots.util.driver_util as d_util
@@ -13,6 +20,7 @@ import chatbots.util.capsule_util as c_util
 
 from datetime import datetime
 import pickle
+
 ### Function that tries to get the name for a new person. A while is used till the user is happy.
 ### From the name a unique ID *human_id* is created by adding the time_stamp
 ### We store the face embeddings in the friend_embeddings folder using the unique ID *human_id*
@@ -20,143 +28,169 @@ import pickle
 ### Human_name is used to address the user, and human_id is used to store properties of the user
 
 
-def get_a_name_and_id (scenario: Scenario, agent:str):
-        confirm = ""
-        human_name = ""
-        human_id=""
-        while confirm.lower().find("yes")==-1:
-            ### We take the response from the user and store it as a text signal
-            utterance = input("\n")
-            textSignal = d_util.create_text_signal(scenario, utterance)
-            scenario.append_signal(textSignal)
-            print(utterance)
-            #### We hack the response to find the name of our new fiend
-            #### This name needs to be set in the scenario and assigned to the global variable human
-            human_name = " ".join([foo.title() for foo in utterance.strip().split()])
-            human_name = "_".join(human_name.split())
-        
-            response = (f"So your name is {human_name}?")
-            print(f"{agent}: {response}")
-            textSignal = d_util.create_text_signal(scenario, response)
-            scenario.append_signal(textSignal)
-            
-            ### We take the response from the user and store it as a text signal
-            confirm = input("\n")
-            textSignal = d_util.create_text_signal(scenario, confirm)
-            scenario.append_signal(textSignal)
-            
-        current_time = str(datetime.now().microsecond)
-        human_id = human_name+"_t_"+current_time
-        return human_name, human_id
+def get_a_name_and_id(scenario: Scenario, agent: str):
+    confirm = ""
+    human_name = ""
+    human_id = ""
+    while confirm.lower().find("yes") == -1:
+        ### We take the response from the user and store it as a text signal
+        utterance = input("\n")
+        textSignal = d_util.create_text_signal(scenario, utterance)
+        scenario.append_signal(textSignal)
+        print(utterance)
+        #### We hack the response to find the name of our new fiend
+        #### This name needs to be set in the scenario and assigned to the global variable human
+        human_name = " ".join([foo.title() for foo in utterance.strip().split()])
+        human_name = "_".join(human_name.split())
 
-def get_to_know_person(scenario: Scenario, agent:str, gender:str, age: str, uuid_name: str, embedding, friends_path:str):
-        ### This is a stranger
-        ### We create the agent response and store it as a text signal
-        human_name = "Stranger"
-        response = (
-            f"Hi there. We haven't met. I only know that \n"
-            f"your estimated age is {age} \n and that your estimated gender is "
-            f"{gender}. What's your name?"
-        )
+        response = f"So your name is {human_name}?"
         print(f"{agent}: {response}")
         textSignal = d_util.create_text_signal(scenario, response)
         scenario.append_signal(textSignal)
-        
-        human_name, human_id =get_a_name_and_id(scenario, agent)
- 
-        #### We create the embedding
-        to_save = {"uuid": uuid_name["uuid"], "embedding": embedding}
-        new_friends_embedding_path = friends_path+f"/{human_id}.pkl"
-        #print("New friend embedding file:",new_friends_embedding_path)
-        with open(new_friends_embedding_path, "wb") as stream:
-            pickle.dump(to_save, stream)
-         
-        ### The system responds to the processing of the new name input and stores it as a textsignal
-        response = f": Nice to meet you, {HUMAN_NAME}"
-        print(f"{AGENT}: {response}\n")
-        textSignal = d_util.create_text_signal(scenario, response)
+
+        ### We take the response from the user and store it as a text signal
+        confirm = input("\n")
+        textSignal = d_util.create_text_signal(scenario, confirm)
         scenario.append_signal(textSignal)
-        
-        return human_id, human_name, textSignal
-    
+
+    current_time = str(datetime.now().microsecond)
+    human_id = human_name + "_t_" + current_time
+    return human_name, human_id
+
+
+def get_to_know_person(
+    scenario: Scenario,
+    agent: str,
+    gender: str,
+    age: str,
+    uuid_name: str,
+    embedding,
+    friends_path: str,
+):
+    ### This is a stranger
+    ### We create the agent response and store it as a text signal
+    human_name = "Stranger"
+    response = (
+        f"Hi there. We haven't met. I only know that \n"
+        f"your estimated age is {age} \n and that your estimated gender is "
+        f"{gender}. What's your name?"
+    )
+    print(f"{agent}: {response}")
+    textSignal = d_util.create_text_signal(scenario, response)
+    scenario.append_signal(textSignal)
+
+    human_name, human_id = get_a_name_and_id(scenario, agent)
+
+    #### We create the embedding
+    to_save = {"uuid": uuid_name["uuid"], "embedding": embedding}
+    new_friends_embedding_path = friends_path + f"/{human_id}.pkl"
+    # print("New friend embedding file:",new_friends_embedding_path)
+    with open(new_friends_embedding_path, "wb") as stream:
+        pickle.dump(to_save, stream)
+
+    ### The system responds to the processing of the new name input and stores it as a textsignal
+    response = f": Nice to meet you, {human_name}"
+    print(f"{agent}: {response}\n")
+    textSignal = d_util.create_text_signal(scenario, response)
+    scenario.append_signal(textSignal)
+
+    return human_id, human_name, textSignal
 
 
 ### Function that creates capsules for the basic properties of a friend: name, age and gender.
 ### The capsules are sent to the BRAIN. Thoughts are caught and returned for each property.
 
-def add_new_name_age_gender_to_brain (scenario: Scenario, 
-                  place_id:str, 
-                  location: str, 
-                  human_id: str,
-                  textSignal: TextSignal,
-                  imageSignal: ImageSignal,
-                  age: str,
-                  gender: str,
-                  human_name: str,
-                  my_brain:LongTermMemory):
+
+def add_new_name_age_gender_to_brain(
+    scenario: Scenario,
+    place_id: str,
+    location: str,
+    human_id: str,
+    textSignal: TextSignal,
+    imageSignal: ImageSignal,
+    age: str,
+    gender: str,
+    human_name: str,
+    my_brain: LongTermMemory,
+):
     age_thoughts = ""
     gender_thoughts = ""
     name_thoughts = ""
 
-
     if human_name:
         # A triple was extracted so we compare it elementwise
-        capsule = c_util.scenario_utterance_to_capsule(scenario, 
-                                                                  place_id,
-                                                                  location,
-                                                                  textSignal,
-                                                                  human_id,
-                                                                  human_id,
-                                                                  "label",
-                                                                  human_name)
+        capsule = c_util.scenario_utterance_to_capsule(
+            scenario,
+            place_id,
+            location,
+            textSignal,
+            human_id,
+            human_id,
+            "label",
+            human_name,
+        )
 
         name_thoughts = my_brain.update(capsule, reason_types=True, create_label=True)
-        print('Name capsule:', capsule)
-
+        print("Name capsule:", capsule)
 
     if age:
         # A triple was extracted so we compare it elementwise
-        capsule = c_util.scenario_image_triple_to_capsule(scenario, 
-                                                                  place_id,
-                                                                  location,
-                                                                  imageSignal,
-                                                                  "front_camera", 
-                                                                  human_id,
-                                                                  "age",
-                                                                  str(age))
+        capsule = c_util.scenario_image_triple_to_capsule(
+            scenario,
+            place_id,
+            location,
+            imageSignal,
+            "front_camera",
+            human_id,
+            "age",
+            str(age),
+        )
 
         age_thoughts = my_brain.update(capsule, reason_types=True, create_label=False)
-        print('Age capsule:', capsule)
+        print("Age capsule:", capsule)
 
     if gender:
-        capsule = c_util.scenario_image_triple_to_capsule(scenario, 
-                                                                  place_id,
-                                                                  location,
-                                                                  imageSignal,
-                                                                  "front_camera", 
-                                                                  human_id,
-                                                                  "gender",
-                                                                  gender)
+        capsule = c_util.scenario_image_triple_to_capsule(
+            scenario,
+            place_id,
+            location,
+            imageSignal,
+            "front_camera",
+            human_id,
+            "gender",
+            gender,
+        )
 
         gender_thoughts = my_brain.update(capsule, reason_types=True, create_label=True)
-        print('Gender capsule:', capsule)
+        print("Gender capsule:", capsule)
 
-    return name_thoughts, age_thoughts, gender_thoughts    
+    return name_thoughts, age_thoughts, gender_thoughts
 
 
-def add_new_name_to_brain (scenario: Scenario, 
-                  place_id:str, 
-                  location: str, 
-                  human_id: str,
-                  textSignal: TextSignal,
-                  human_name: str,
-                  my_brain:LongTermMemory):
+def add_new_name_to_brain(
+    scenario: Scenario,
+    place_id: str,
+    location: str,
+    human_id: str,
+    textSignal: TextSignal,
+    human_name: str,
+    my_brain: LongTermMemory,
+):
     name_thoughts = ""
 
     # A triple was extracted so we compare it elementwise
-    capsule = c_util.scenario_utterance_to_capsule(scenario,place_id,location, textSignal,human_id,human_id,"label", human_name)
+    capsule = c_util.scenario_utterance_to_capsule(
+        scenario,
+        place_id,
+        location,
+        textSignal,
+        human_id,
+        human_id,
+        "label",
+        human_name,
+    )
 
     name_thoughts = my_brain.update(capsule, reason_types=True, create_label=True)
-    print('Name capsule:', capsule)
+    print("Name capsule:", capsule)
 
-    return name_thoughts    
+    return name_thoughts
